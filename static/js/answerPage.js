26cb3b7e121b733fa7d360c8f9ab15e363672d75
M Manogna <M.Manogna@citrix.com>
2017-07-08 11:06:00 +0530

    Initial files

---
+++ static/js/answerPage.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+	$('#ask_now_id').on('submit', function () {
+      $.ajax();
+    });
+
+    $('.ckbox label').on('click', function () {
+      $(this).parents('tr').toggleClass('selected');
+    });
+
+    $('.btn-filter').on('click', function () {
+      var $target = $(this).data('target');
+      if ($target != 'all') {
+        $('.table tr').css('display', 'none');
+        $('.table tr[data-status="' + $target + '"]').fadeIn('slow');
+      } else {
+        $('.table tr').css('display', 'none').fadeIn('slow');
+      }
+    });
+
+ });
